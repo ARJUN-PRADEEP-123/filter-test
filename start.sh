7a9998dd4b73778ffa9cc466cf513c902a126069
ARJUN-PRADEEP-123 <93504468+ARJUN-PRADEEP-123@users.noreply.github.com>
2022-02-09 14:52:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TELEZEN/jennie-tr-test2.git /jennie-tr-test2
+  git clone https://github.com/ARJUN-PRADEEP-123/filter-test.git /filter-test
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /jennie-tr-test2
+  git clone $UPSTREAM_REPO /filter-test
 fi
-cd /jennie-tr-test2
+cd /filter-test
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
